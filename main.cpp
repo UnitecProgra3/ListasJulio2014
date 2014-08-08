8aac93acfb9680f1a09a050693e44cb5e6eeafeb
Turupawn <turupawn@userr>
2014-08-07 19:52:55 -0600

    primer commit

--- main.cpp
+++ main.cpp
@@ -22,6 +22,32 @@ public:
     {
         inicio=NULL;
     }
+
+    bool insertar(Nodo*nuevo,int pos)
+    {
+        if(pos == 0)
+        {
+            nuevo->sig = inicio;
+            inicio = nuevo;
+        }else
+        {
+            Nodo* temp = inicio;
+            for(int i =0;i<pos-1;i++)
+            {
+                temp = temp->sig;
+                if(temp==NULL)
+                {
+                    cout<<"Posicion invalida"<<endl;
+                    return false;
+                }
+            }
+            nuevo->sig = temp->sig;
+            temp->sig = nuevo;
+        }
+
+        return true;
+    }
+
     void agregar(Nodo*nuevo)
     {
         if(inicio==NULL)
@@ -44,18 +70,92 @@ public:
             cout<<temp->num<<endl;
         }
     }
-    bool buscar(int num)
+    bool existe(int num)
     {
+        Nodo*temp = inicio;
+        while(temp != NULL)
+        {
+            if(num == temp->num)
+                return true;
+            temp = temp->sig;
+        }
+        return false;
+    }
+    void borrar(int pos)
+    {
+        if(pos==0)
+        {
+            Nodo*temp2 = inicio;
+            inicio = inicio->sig;
+            delete temp2;
+        }else
+        {
+            Nodo* temp = inicio;
+            for(int i=0;i<pos-1;i++)
+            {
+                temp = temp->sig;
+            }
+            Nodo* temp2 = temp->sig;
+            temp->sig = temp->sig->sig;
+            delete temp2;
+        }
+    }
+    void borrarUltimo()
+    {
+        if(inicio==NULL)
+        {
+            cout<<"Lista vacia"<<endl;
+        }
+        else if(inicio->sig == NULL)
+        {
+            delete inicio;
+            inicio = NULL;
+        }else
+        {
+            Nodo*temp = inicio;
+            for(;temp->sig->sig != NULL;
+                    temp = temp->sig);
+            delete temp->sig;
+            temp->sig = NULL;
+        }
+    }
+    void borrarTodo()
+    {
+    }
+    void intercambiar(int pos1, int pos2)
+    {
+    }
 
+    void invertir()
+    {
+    }
+
+    void ordenar()
+    {
+    }
+
+    void mezclar(Lista*l)
+    {
+    }
+
+    void unificar()
+    {
     }
 };
 
 int main()
 {
-    Lista *l = new Lista();
-    l->agregar(new Nodo(10));
-    l->agregar(new Nodo(20));
-    l->agregar(new Nodo(30));
-    l->imprimir();
+    Lista l;
+    l.agregar(new Nodo(10));
+    l.agregar(new Nodo(20));
+    l.agregar(new Nodo(30));
+    l.agregar(new Nodo(50));
+    l.borrar(0);
+    while(true)
+    {
+        l.agregar(new Nodo(50));
+//        l.borrar(2);
+    }
+    l.imprimir();
     return 0;
 }
